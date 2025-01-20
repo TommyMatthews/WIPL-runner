bbf8c6e61b5da8ee634117fd0ce2d1198b9622f4
flyupinthesky <68277174+flyupinthesky@users.noreply.github.com>
2025-01-20 15:31:40 +0000

    First working test of using WIPL py to read field values from an ra1 file

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import wiplpy.WiplInterface
+import wiplpy.WResults
 
-print("Worked")
+PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\vertical_alternate_run_test_dummy\M_Wing_1_100_V_X_alternate_run"
+
+WIPLDInstallDirectory = r"C:\Program Files\WIPL-D\WIPL-D Pro 13.02.00"
+
+#WIPLD = wiplpy.WiplInterface(WIPLDInstallDirectory, "wipldpro")
+
+far_field = wiplpy.WResults.InitializeFFResults(PROJECT_PATH)
+
+theta = far_field.GetThetaPoints()[0]
+frequency = far_field.GetFrequencies()[0]
+
+print(theta, frequency)
+phi_results = far_field.GetPhiPoints()
+
+#print(phi_results)
+
+results_by_phi =  far_field.GetYData('Phi-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : theta, "Frequency" : frequency,"Excitation" : 1})
+
+print(results_by_phi)
+#think I need to try this on a run that has been run using a built in set of excitations rather than a sweep.
