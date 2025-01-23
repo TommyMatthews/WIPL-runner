20a603ebb1de3a26967e3775d0bf8d7bb0280158
flyupinthesky <68277174+flyupinthesky@users.noreply.github.com>
2025-01-23 08:47:24 +0000

    Gets pkl working

--- running/duplicate_p_martin_results/100_X_H.py
+++ running/duplicate_p_martin_results/100_X_H.py
@@ -1,22 +1,7 @@
 import wiplpy.WiplInterface
 import wiplpy.WResults
-import WIPL_PYTHON_ANALYSIS
-import joblib
+import pandas as pd
 
-PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\horizontal_alternate_run\M_Wing_1_100_H_X"
-WIPLDInstallDirectory = r"C:\WIPL-D Pro CAD 2024"
-
-pro = wiplpy.WiplInterface.InitializeWIPLDSuite(WIPLDInstallDirectory, "wipldpro")
-
-print("Starting run")
-pro.Run(PROJECT_PATH)
-print("Run complete")
-
-print("Initialising far field results")
-far_field = wiplpy.WResults.InitializeFFResults(PROJECT_PATH)
-print("Saving far field results")
-
-joblib.dump(far_field, "100_X_H.pkl")
 
 
 
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import wiplpy.WiplInterface
 import wiplpy.WResults
+import pickle
 
 PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\vertical_alternate_run_test_dummy\M_Wing_1_100_V_X_alternate_run"
 
-WIPLDInstallDirectory = r"C:\Program Files\WIPL-D\WIPL-D Pro 13.02.00"
+#WIPLDInstallDirectory = r"C:\Program Files\WIPL-D\WIPL-D Pro 13.02.00"
 
 #WIPLD = wiplpy.WiplInterface(WIPLDInstallDirectory, "wipldpro")
 
@@ -18,6 +19,10 @@ phi_results = far_field.GetPhiPoints()
 #print(phi_results)
 
 results_by_phi =  far_field.GetYData('Phi-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : theta, "Frequency" : frequency,"Excitation" : 1})
+
+# Pickle the far_field object
+with open('X_V_alternate_run_results.pkl', 'wb') as f:
+    pickle.dump(far_field, f)
 
 print(results_by_phi)
 #think I need to try this on a run that has been run using a built in set of excitations rather than a sweep.
