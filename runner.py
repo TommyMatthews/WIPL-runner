f942c0cba3f156973488b79ad9e93fd70e2441fa
flyupinthesky <68277174+flyupinthesky@users.noreply.github.com>
2025-01-23 12:29:18 +0000

    re running X_H

--- runner.py
+++ runner.py
@@ -2,12 +2,15 @@ import wiplpy.WiplInterface
 import wiplpy.WResults
 from FFObjToDict import FFObjToDictConverter
 
-PROJECT_PATH = r"E:\Working_Copy\Bernard_Ellipsoid_Comparison\Bernard\LepidopteraNoctuidaeMoth_12_Full_M_0250_0375_94_MirkLBG_ChenMeanandLegs"
+#PROJECT_PATH = r"E:\Working_Copy\Bernard_Ellipsoid_Comparison\Bernard\LepidopteraNoctuidaeMoth_12_Full_M_0250_0375_94_MirkLBG_ChenMeanandLegs"
+PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\horizontal_alternate_run\M_Wing_1_100_H_X"
+
 
 WIPLDInstallDirectory = r"C:\WIPL-D Pro CAD 2024"
 
-BASE_FILE_NAME = "FullSize_B_freq_sweep"
-SAVE_PATH = r"C:\Users\NCAS\Documents\Tommy\Bernard_run_outputs\\"
+BASE_FILE_NAME = "X_H_alternate_run"
+#BASE_FILE_NAME = "FullSize_B_freq_sweep"
+#SAVE_PATH = r"C:\Users\NCAS\Documents\Tommy\Bernard_run_outputs\\"
 
 pro= wiplpy.WiplInterface.InitializeWIPLDSuite(WIPLDInstallDirectory, "wipldpro")
 
@@ -23,13 +26,13 @@ if len(frequencies) > 1:
     for counter, frequency in enumerate(far_field.GetFrequencies()):
         print(f'Saving frequency number {counter}, value {frequency} GHz' )
         results_extractor = FFObjToDictConverter(far_field, theta, frequency)
-        results_extractor.save_output_dict(SAVE_PATH + f"{BASE_FILE_NAME}_f{counter}_dict.pkl")
+        results_extractor.save_output_dict(f"{BASE_FILE_NAME}_f{counter}_dict.pkl")
 
 else:
     print("Only one frequency present")
     frequency = frequencies[0]
     results_extractor = FFObjToDictConverter(far_field, theta, frequency)
-    results_extractor.save_output_dict(SAVE_PATH + f"{BASE_FILE_NAME}_dict.pkl")
+    results_extractor.save_output_dict(f"{BASE_FILE_NAME}_dict.pkl")
 
 
 
