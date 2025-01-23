eae62f716cf22b37179524efb8d1dae0395ce6fc
Thomas Matthews <tommyam98@gmail.com>
2025-01-23 09:35:46 +0000

    Writes first pass at results extractor

--- FFObjToSpreadsheet.py
+++ FFObjToSpreadsheet.py
@@ -10,16 +10,57 @@ class ResultsExtractor():
         self.frequency = frequency
 
     def extract_phi_results(self):
-        "Returns complex phi results"
-        pass
+        re_Ephi = self.ffobject.GetYData('Phi-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        im_Ephi = self.ffobject.GetYData('Phi-component',"Im", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        
+        return re_Ephi, im_Ephi
     
     def extract_theta_results(self):
-        "Returns complex theta results"
-        pass
+        re_Etheta = self.ffobject.GetYData('Theta-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        im_Etheta = self.ffobject.GetYData('Theta-component',"Im", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        
+        return re_Etheta, im_Etheta
 
-    #also do total RCS 
+    def correct_rcs(self, rcs):
+        wavelength = 30 / self.frequency
+        corrected_rcs = rcs * wavelength**2
+        return corrected_rcs 
 
-    #generate output df with all of these
+    def extract_total_rcs(self):
+        total_rcs = self.ffobject.GetYData('Total',"RCS", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        corrected_rcs = self.correct_rcs(total_rcs)
+        return total_rcs, corrected_rcs
+    
+    def generate_output_df(self):
 
-with open('X_V_alternate_run_results.pkl', 'rb') as file:
-    ffobject = pickle.load(file)
+        re_Ephi, im_Ephi = self.extract_phi_results()
+        re_Etheta, im_Etheta = self.extract_theta_results()
+        Ephi = re_Ephi + j*im_Ephi
+        Etheta = re_Etheta + j*im_Etheta
+
+        total_rcs, corrected_rcs = self.extract_total_rcs()
+
+        output_df = pd.DataFrame({
+            'phi': self.ffobject.GetPhiPoints(),
+            'Re_Ephi': re_Ephi,
+            'Im_Ephi': im_Ephi,
+            'Re_Etheta': re_Etheta,
+            'Im_Etheta': im_Etheta,
+            'Total_RCS': total_rcs,
+            'Corrected_RCS': corrected_rcs
+        })
+
+        output_df.attrs['theta'] = self.theta
+        output_df.attrs['frequency'] = self.frequency
+
+        return output_df
+if __name__ == "__main__":
+    with open('X_V_alternate_run_results.pkl', 'rb') as file:
+        ffobject = pickle.load(file)
+
+    theta = ffobject.GetThetaPoints()[0]
+    frequency = ffobject.GetFrequencies()[0]
+
+    results_extractor = ResultsExtractor(ffobject, theta, frequency)
+
+    results_extractor.generate_output_df().to_csv('X_V_test.csv')
