472ff30d988e042799f2ab30bb7f936ed1dcaf6e
Thomas Matthews <tommyam98@gmail.com>
2025-01-24 16:09:23 +0000

    Black formatting

--- FFObjToDict.py
+++ FFObjToDict.py
@@ -1,7 +1,8 @@
 import wiplpy.WResults
 import pickle
 
-class FFObjToDictConverter():
+
+class FFObjToDictConverter:
 
     def __init__(self, ffobject, theta, frequency):
         self.ffobject = ffobject
@@ -11,22 +12,47 @@ class FFObjToDictConverter():
         self.OUTPUT_GENERATED = False
 
     def extract_phi_results(self):
-        re_Ephi = self.ffobject.GetYData('Phi-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
-        im_Ephi = self.ffobject.GetYData('Phi-component',"Im", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
-        
+        re_Ephi = self.ffobject.GetYData(
+            "Phi-component",
+            "Re",
+            XaxisLabel="phi",
+            Cuts={"Theta": self.theta, "Frequency": self.frequency, "Excitation": 1},
+        )
+        im_Ephi = self.ffobject.GetYData(
+            "Phi-component",
+            "Im",
+            XaxisLabel="phi",
+            Cuts={"Theta": self.theta, "Frequency": self.frequency, "Excitation": 1},
+        )
+
         return re_Ephi, im_Ephi
-    
+
     def extract_theta_results(self):
-        re_Etheta = self.ffobject.GetYData('Theta-component',"Re", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
-        im_Etheta = self.ffobject.GetYData('Theta-component',"Im", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
-        
+        re_Etheta = self.ffobject.GetYData(
+            "Theta-component",
+            "Re",
+            XaxisLabel="phi",
+            Cuts={"Theta": self.theta, "Frequency": self.frequency, "Excitation": 1},
+        )
+        im_Etheta = self.ffobject.GetYData(
+            "Theta-component",
+            "Im",
+            XaxisLabel="phi",
+            Cuts={"Theta": self.theta, "Frequency": self.frequency, "Excitation": 1},
+        )
+
         return re_Etheta, im_Etheta
 
     def extract_total_rcs(self):
-        total_rcs = self.ffobject.GetYData('Total',"RCS", XaxisLabel='phi',Cuts ={"Theta" : self.theta, "Frequency" : self.frequency,"Excitation" : 1})
+        total_rcs = self.ffobject.GetYData(
+            "Total",
+            "RCS",
+            XaxisLabel="phi",
+            Cuts={"Theta": self.theta, "Frequency": self.frequency, "Excitation": 1},
+        )
 
         return total_rcs
-    
+
     def generate_output_dict(self):
 
         re_Ephi, im_Ephi = self.extract_phi_results()
@@ -35,37 +61,37 @@ class FFObjToDictConverter():
         total_rcs = self.extract_total_rcs()
 
         self.output_dict = {
-            'phi': self.ffobject.GetPhiPoints(),
-            'Re_Ephi': re_Ephi,
-            'Im_Ephi': im_Ephi,
-            'Re_Etheta': re_Etheta,
-            'Im_Etheta': im_Etheta,
-            'Total_RCS': total_rcs,
+            "phi": self.ffobject.GetPhiPoints(),
+            "Re_Ephi": re_Ephi,
+            "Im_Ephi": im_Ephi,
+            "Re_Etheta": re_Etheta,
+            "Im_Etheta": im_Etheta,
+            "Total_RCS": total_rcs,
         }
 
-        self.output_dict['theta'] = self.theta
-        self.output_dict['frequency'] = self.frequency
+        self.output_dict["theta"] = self.theta
+        self.output_dict["frequency"] = self.frequency
 
         self.OUTPUT_GENERATED = True
-    
+
     def get_output_dict(self):
 
         if not self.OUTPUT_GENERATED:
             self.generate_output_dict()
 
         return self.output_dict
-    
+
     def save_output_dict(self, file_path):
 
         if not self.OUTPUT_GENERATED:
             self.generate_output_dict()
 
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             pickle.dump(self.output_dict, f)
-    
+
 
 if __name__ == "__main__":
-    with open('X_V_alternate_run_results.pkl', 'rb') as file:
+    with open("X_V_alternate_run_results.pkl", "rb") as file:
         ffobject = pickle.load(file)
 
     theta = ffobject.GetThetaPoints()[0]
@@ -73,4 +99,4 @@ if __name__ == "__main__":
 
     results_extractor = FFObjToDictConverter(ffobject, theta, frequency)
 
-    results_extractor.save_output_dict('X_V_alternate_run_results.pkl')
+    results_extractor.save_output_dict("X_V_alternate_run_results.pkl")
--- runner.py
+++ runner.py
@@ -3,16 +3,16 @@ import wiplpy.WResults
 from FFObjToDict import FFObjToDictConverter
 
 PROJECT_PATH = r"E:\Working_Copy\Bernard_Ellipsoid_Comparison\Bernard\LepidopteraNoctuidaeMoth_12_Full_M_0250_0375_94_MirkLBG_ChenMeanandLegs"
-#PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\horizontal_alternate_run\M_Wing_1_100_H_X"
-#PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\S_band\horizontal_alternate_run\M_Wing_1_100_H_alternate_run"
+# PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\X_band\horizontal_alternate_run\M_Wing_1_100_H_X"
+# PROJECT_PATH = r"C:\Users\NCAS\Documents\Tommy\purple_martin_work\P_Martin2\S_band\horizontal_alternate_run\M_Wing_1_100_H_alternate_run"
 
 WIPLDInstallDirectory = r"C:\WIPL-D Pro CAD 2024"
 
-#BASE_FILE_NAME = "S_H_alternate_run"
+# BASE_FILE_NAME = "S_H_alternate_run"
 BASE_FILE_NAME = "FullSize_B_freq_sweep"
 SAVE_PATH = r"C:\Users\NCAS\Documents\Tommy\Bernard_run_outputs\\"
 
-pro= wiplpy.WiplInterface.InitializeWIPLDSuite(WIPLDInstallDirectory, "wipldpro")
+pro = wiplpy.WiplInterface.InitializeWIPLDSuite(WIPLDInstallDirectory, "wipldpro")
 
 pro.Run(PROJECT_PATH)
 
@@ -24,16 +24,14 @@ frequencies = far_field.GetFrequencies()
 if len(frequencies) > 1:
     print("Multiple frequencies present")
     for counter, frequency in enumerate(far_field.GetFrequencies()):
-        print(f'Saving frequency number {counter}, value {frequency} GHz' )
+        print(f"Saving frequency number {counter}, value {frequency} GHz")
         results_extractor = FFObjToDictConverter(far_field, theta, frequency)
-        results_extractor.save_output_dict(SAVE_PATH + f"{BASE_FILE_NAME}_f{counter}_dict.pkl")
+        results_extractor.save_output_dict(
+            SAVE_PATH + f"{BASE_FILE_NAME}_f{counter}_dict.pkl"
+        )
 
 else:
     print("Only one frequency present")
     frequency = frequencies[0]
     results_extractor = FFObjToDictConverter(far_field, theta, frequency)
     results_extractor.save_output_dict(SAVE_PATH + f"{BASE_FILE_NAME}_dict.pkl")
-
-
-
-    
